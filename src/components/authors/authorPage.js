83661de44014c22ea5f22d1a06e5bb051363ca92
Siya Nkosi <siya.nkosi@fnb.co.za>
2018-09-06 21:37:39 +0200

    initial commit

---
+++ src/components/authors/authorPage.js
@@ -0,0 +1,42 @@
+"use strict";
+
+var React = require('react');
+var Router = require('react-router');
+var Link = Router.Link;
+var AuthorStore = require('../../stores/authorStore');
+var AuthorActions = require('../../actions/authorActions');
+var AuthorList = require('../authors/authorList');
+
+var Authors = React.createClass({
+    getInitialState: function(){
+        return {
+            authors: AuthorStore.getAllAuthors()
+        };
+    },
+
+    componentWillMount: function(){
+        console.log('add listener onChange event');
+        AuthorStore.addChangeListener(this._onChange);
+    },
+
+    componentWillUnmount: function(){
+        console.log('remove listener onChange event');
+        AuthorStore.removeChangeListener(this._onChange);
+    },
+
+    _onChange: function(){
+        this.setState({authors: AuthorStore.getAllAuthors() });
+    },
+
+    render: function(){
+        return (
+            <div>
+                <h1>Authors</h1>
+                <Link to="addAuthor" className="btn btn-default">Add Author</Link>
+                <AuthorList authors={this.state.authors} />                
+            </div>
+        );
+    }
+});
+
+module.exports = Authors;
---
+++ src/main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var React = require('react');
+var Router = require('react-router');
+var routes = require('./routes');
+var InitalizeActions = require('./actions/initializeActions');
+
+InitalizeActions.initApp();
+
+Router.run(routes, function(Handler){
+    React.render(<Handler />, document.getElementById('app'));
+});
