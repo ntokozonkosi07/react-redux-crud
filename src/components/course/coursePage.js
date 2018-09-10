6c4abe6ed77a2c981e26514bbbd1a5e246df7ebe
Siya Nkosi <siya.nkosi@fnb.co.za>
2018-09-10 19:39:10 +0200

    FNBPVN-1965 added courses page

---
+++ src/components/course/coursePage.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var React = require('react');
+
+var CoursePage = React.createClass({
+    render: function(){
+        return (
+            <div>
+                <h1>Courses</h1>
+                              
+            </div>
+        );
+    }
+});
+
+module.exports = CoursePage;
