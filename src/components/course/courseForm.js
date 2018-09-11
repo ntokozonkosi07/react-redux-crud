f94e094d5a5740c7eeabc875597ea2ed17987544
Siya Nkosi <siya.nkosi@fnb.co.za>
2018-09-11 08:22:25 +0200

    adding routes

---
+++ src/components/course/courseForm.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var React = require('react');
+
+var CourseForm = React.createClass({
+    render: function(){
+        return (
+            <div>...Form Here...</div>
+        );
+    }
+});
+
+module.exports = CourseForm;
